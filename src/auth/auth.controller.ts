df0d3673932b7396a778374d0369ddb3e3adeca8
salaveti.Reddy <6o1QLc5hi1blT0MvrtASl0e9nsFoWLJOL1J5xWSNH7ZaYfv0ReAbJQQJ99AKACAAAAAq259BAAASAZDOFcvr>
2026-02-18 11:27:53 +0530

    removed unwanted code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { SignInCredentialsDto } from './dto/signin-credentials.dto';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,11 +17,5 @@ export class AuthController {
     @Body() signInCredentialsDto: SignInCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(signInCredentialsDto);
-  }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log('req', req);
   }
 }
